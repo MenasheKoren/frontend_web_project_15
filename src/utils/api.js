9368982aeecd8a0f645dbee6397fa7a9c1a90474
menashekoren <menashekoren@gmail.com>
2022-02-07 08:22:08 +0200

    Initial commit

---
+++ src/utils/api.js
@@ -0,0 +1,84 @@
+import { token } from './auth';
+
+
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getInitialCards() {
+    return customFetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    });
+  }
+
+  getUserInfo() {
+    return customFetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    });
+  }
+
+  editUserInfo(data) {
+    return customFetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+      method: 'PATCH',
+      body: JSON.stringify(data),
+    });
+  }
+
+  editAvatar(data) {
+    return customFetch(`${this._baseUrl}/users/me/avatar`, {
+      headers: this._headers,
+      method: 'PATCH',
+      body: JSON.stringify({
+        avatar: data,
+      }),
+    });
+  }
+
+  createCard(data) {
+    return customFetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+  }
+
+  deleteCard(cardId) {
+    return customFetch(`${this._baseUrl}/cards/${cardId}`, {
+      headers: this._headers,
+      method: 'DELETE',
+    });
+  }
+
+  addLikes(cardId) {
+    return customFetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      headers: this._headers,
+      method: 'PUT',
+    });
+  }
+
+  removeLikes(cardId) {
+    return customFetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      headers: this._headers,
+      method: 'DELETE',
+    });
+  }
+}
+
+const customFetch = (url, headers) =>
+  fetch(url, headers).then((res) =>
+    res.ok ? res.json() : Promise.reject(res.statusText)
+  );
+
+const api = new Api({
+  baseUrl: 'https://api.menashekoren.students.nomoreparties.sbs',
+  // baseUrl: 'http://localhost:3000/',
+  headers: {
+    authorization: 'Bearer ${token}',
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
